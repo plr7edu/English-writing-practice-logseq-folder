a062262b28dfa18fd6185a2fbfa1727eef154afd
plr7edu <104339100+plr7edu@users.noreply.github.com>
2023-01-29 18:58:54 +0530

    Initial Commit

---
+++ writing-mode-script-windows/writing-mode-windows-10.bat
@@ -0,0 +1,5 @@
+@echo off
+start /min "" "C:\Program Files (x86)\AIMP\AIMP.exe"
+cd "C:\Users\PLR\AppData\Local\Logseq"
+start Logseq.exe
+exit
---
+++ writing-mode-script-windows/writing-mode-windows-8.1.bat
@@ -0,0 +1,5 @@
+@echo off
+start /min "" "C:\Program Files (x86)\AIMP\AIMP.exe"
+cd "C:\Users\PLREDU\AppData\Local\Logseq"
+start Logseq.exe
+exit
